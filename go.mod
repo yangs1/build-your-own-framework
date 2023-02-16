a34432cceca9f17e19dd7be83c4d6ecb6ee1ce18
Young <miss_ysp@163.com>
2023-02-16 17:27:57 +0800

    init framework dirs

---
+++ go.mod
@@ -0,0 +1,10 @@
+module golsf
+
+go 1.18
+
+require (
+	framework v0.0.0
+)
+replace (
+	framework => "./framework"
+)
--- main.go
+++ main.go
@@ -1,39 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"framework"
 	"log"
 	"net/http"
-	"sync"
 )
 
 func main() {
-
-	http.Handle("/test", new(countHandle))
-
-	http.HandleFunc("/bar", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println("test")
-		fmt.Fprintf(writer, "hello, %q", request.URL.Path)
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-type countHandle struct {
-	mu sync.Mutex //guard n
-	n  int
-}
-
-func newCon() *countHandle {
-	return &countHandle{
-		n: 0,
+	server := &http.Server{
+		Handler: framework.NewCore(),
+		Addr:    ":8080",
 	}
-}
 
-func (c *countHandle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	c.n++
-	fmt.Fprintf(w, "Count is %d\n", c.n)
+	log.Fatal(server.ListenAndServe())
 }
